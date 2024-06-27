11d18964e3eb7a29dcf1a7e9a0f85dbe1b5dde53
KarenBareiro <karennbenitez@hotmail.com>
2024-06-26 21:24:10 -0400

    second commit

--- src/componentes/App/App.jsx
+++ src/componentes/App/App.jsx
@@ -9,35 +9,57 @@ class App extends Component {
         name: "Doe, Jane",
         age: 24,
         hairColor: "Black",
+        buttonName: "Jane",
       },
       {
         name: "Smith, John",
         age: 88,
         hairColor: "Brown",
+        buttonName: "John",
       },
       {
         name: "Fillmore, Millard",
         age: 50,
         hairColor: "Brown",
+        buttonName: "Millard",
       },
       {
         name: "Smith, Maria",
         age: 62,
         hairColor: "Brown",
+        buttonName: "Maria",
       }]
-
     }
-
   }
+  increaseAge = (name) => {
+    // Encuentra la persona por su nombre y actualiza la edad
+    const updatedList = this.state.list.map((person) => {
+      if (person.name === name) {
+        return {
+          ...person,
+          age: person.age + 1,
+        };
+      }
+      return person;
+    });
 
+    this.setState({ list: updatedList });
+  };
   render = () => {
     return (
       <>
-        {this.state.list.map((newlist)=>{
-          return(<PersonCard name= {newlist.name} age={newlist.age} hairColor={newlist.hairColor}/>)
-          })}
+        {this.state.list.map((person, index) => (
+          <PersonCard
+            key={index}
+            name={person.name}
+            age={person.age}
+            hairColor={person.hairColor}
+            buttonName={person.buttonName}
+            increaseAge={() => this.increaseAge(person.name)} // Pasar el nombre correcto a increaseAge
+          />
+        ))}
       </>
-    )
+    );
   }
 }
 
--- src/componentes/PersonCard/PersonCard.jsx
+++ src/componentes/PersonCard/PersonCard.jsx
@@ -1,12 +1,19 @@
-const PersonCard = (props) =>{
-    return(
-        <>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Hair Color: {props.hairColor}</p>
-        </>
-    );
-}
+import { Component } from "react";
 
+class PersonCard extends Component{
+    constructor(props){
+        super(props);
+    }
+    render = () =>{
+        return(
+            <>
+            <h2>{this.props.name}</h2>
+            <p>Age: {this.props.age}</p>
+            <p>Hair Color: {this.props.hairColor}</p>
+            <button onClick={this.props.increaseAge}>Birthday button for {this.props.buttonName}</button>
+            </> 
+        )
+    }
+}
 
 export default PersonCard;
